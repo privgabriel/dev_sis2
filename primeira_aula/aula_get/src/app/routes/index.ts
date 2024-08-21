c0b73b4645231536a3c57cc21200d80982ee332d
Gabriel <98443721+privgabriel@users.noreply.github.com>
2024-08-20 21:09:28 -0300

    Delete primeira_aula/aula_get directory

--- primeira_aula/aula_get/src/app/routes/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import UserRouter from "../controllers/UserController";
-
-const routers = Router();
-
-routers.use("/users", UserRouter);
-
-export default routers;
--- primeira_aula/aula_get/src/database/scripts/1.sql
+++
@@ -1,7 +0,0 @@
--- Arquivo: create_database.sql
-
--- Criação do banco de dados
-CREATE DATABASE IF NOT EXISTS projeto_typeorm;
-
--- Definindo o banco de dados criado como o ativo
-USE projeto_typeorm;
